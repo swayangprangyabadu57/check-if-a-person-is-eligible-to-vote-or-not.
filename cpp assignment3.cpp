54712b1682001cc25c971248f89814923ee87e63
swayangprangyabadu57 <91285381+swayangprangyabadu57@users.noreply.github.com>
2021-11-28 17:48:28 +0530

    Here we check if a person is eligible to vote or not.
     C++ program where we check if a person is eligible to vote or not. We ask the user for an input of their age and give them an output which states whether or not they are eligible.

---
+++ cpp assignment3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout << "Enter age oa a user: ";
+	cin >> age;
+	
+	if(age>=18)
+	{
+		cout << "\nYou are eligible for voting";
+	}
+	else
+	{
+	cout << "\nYou are not eligible for voting";
+    }
+    return 0;
+}
+
